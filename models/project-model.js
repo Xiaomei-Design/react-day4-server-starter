811ee2b06022eac71f6af2ed7c68a915d25b4854
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-12-08 00:07:30 +0100

    authentification practice

--- models/project-model.js
+++ models/project-model.js
@@ -6,8 +6,9 @@ const Schema = mongoose.Schema;
 const projectSchema = new Schema({
   title: String,
   description: String,
-  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
+  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
   // owner will be added later on
+  owner: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 const Project = mongoose.model('Project', projectSchema);
